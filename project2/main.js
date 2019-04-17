eabee9709a227a2b37b8e46727df50fa6fe5bf37
Aziza <wako@usc.edu>
2019-04-17 09:34:51 -0700

    added projects

---
+++ project2/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+  var el = $("banana");
+  console.log(element);
+  
+}
---
+++ project2/selection.js
@@ -0,0 +1,10 @@
+function click(401,953){
+    var ev = document.createEvent("MouseEvent");
+    var el = document.elementFromPoint(401,953);
+    ev.initMouseEvent(
+        "click",
+   var result = str.link("semele.html")
+   document.getElementById("demo").innerHTML = result;
+    );
+    el.dispatchEvent(ev);
+}
